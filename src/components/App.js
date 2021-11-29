78ebaa003e262fe17d08aa85c7731d2c414687f6
Hoa Le <Hoa_le9485@yahoo.com>
2021-11-29 12:29:13 -0500

    items question

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
-
+  const [items, setItems] = useState(itemData)
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={itemData} />
+      {/* use props to get acess down to child which is isDarkMode={isDarkMode}  */}
+      {/* use props{callback} to get access from child up to parent
+      i.e; onDarkModeClick is a prop{CallBack} which enable on isDarkMode assign to the Prop{CallBack} to be used in the child component accessing the handleDarkModeClick in the Parent component */}
+      {/* Tldr; props = prop{callBack} to go up the parent component
+          Tldr; props = props to go down to the child component */}
+      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+      <ShoppingList items={items} />
     </div>
   );
 }
